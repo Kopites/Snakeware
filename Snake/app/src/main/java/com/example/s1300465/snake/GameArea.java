ea211517ae4a084228c7700a4671b44a678d8194
Murreey <Murreey@users.noreply.github.com>
2016-02-17 19:10:40 +0000

    Implement basic snake and snake movement

--- Snake/app/src/main/java/com/example/s1300465/snake/GameArea.java
+++ Snake/app/src/main/java/com/example/s1300465/snake/GameArea.java
@@ -12,11 +12,15 @@ import java.util.Random;
 
 public class GameArea extends View {
     private int TARGET_PIXEL_SIZE = 50;
-    private float PIXEL_WIDTH = 50;
-    private float PIXEL_HEIGHT = 50;
+    private float PIXEL_WIDTH;
+    private float PIXEL_HEIGHT;
     private GridTile grid[][];
     private int gridWidth, gridHeight;
     private final Paint paint = new Paint();
+
+    private Direction dir;
+    private int length;
+    private int snakeX, snakeY;
 
     public GameArea(Context context, AttributeSet attrs){
         super(context, attrs);
@@ -25,15 +29,61 @@ public class GameArea extends View {
     @Override
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
-        for(int i = 0; i < gridWidth; i++){
-            for(int j = 0; j < gridHeight; j++){
-                Random rnd = new Random();
-                paint.setColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
-                float pixelLeft = PIXEL_WIDTH * i;
-                float pixelTop = PIXEL_HEIGHT * j;
+
+        for(int x = 0; x < gridWidth; x++){
+            for(int y = 0; y < gridHeight; y++){
+                paint.setColor(Color.TRANSPARENT);
+
+                if(grid[x][y] == GridTile.SnakeHead){
+                    paint.setColor(Color.WHITE);
+                }else if(grid[x][y] == GridTile.SnakePiece){
+                    paint.setColor(Color.WHITE);
+                }else if(grid[x][y] == GridTile.Pickup){
+                    paint.setColor(Color.GREEN);
+                }
+
+                float pixelLeft = (PIXEL_WIDTH * x);
+                float pixelTop = (PIXEL_HEIGHT * y);
                 canvas.drawRect(pixelLeft, pixelTop, pixelLeft + PIXEL_WIDTH, pixelTop + PIXEL_HEIGHT, paint);
             }
         }
+    }
+
+    protected void spawnSnake(int x, int y, Direction dir, int length) throws IndexOutOfBoundsException{
+        this.snakeX = x;
+        this.snakeY = y;
+        this.dir = dir;
+        this.length = length;
+
+        grid[x][y] = GridTile.SnakeHead;
+
+        for(int i = 1; i <= length; i++){
+            switch(dir){
+                case UP:
+                    grid[x][y + i] = GridTile.SnakePiece;
+                    break;
+                case LEFT:
+                    grid[x + i][y] = GridTile.SnakePiece;
+                    break;
+                case RIGHT:
+                    grid[x - i][y] = GridTile.SnakePiece;
+                    break;
+                case DOWN:
+                    grid[x][y - i] = GridTile.SnakePiece;
+                    break;
+            }
+        }
+    }
+
+    protected void clearSnake(){
+        grid = new GridTile[gridWidth][gridHeight];
+    }
+
+    protected int getGridWidth(){
+        return gridWidth;
+    }
+    protected int getGridHeight(){
+        return gridHeight;
     }
 
     @Override
@@ -53,16 +103,36 @@ public class GameArea extends View {
         //Then divide it up and increase the pixel sizes to fit the screen perfectly
         PIXEL_WIDTH = TARGET_PIXEL_SIZE + xRemainder/gridWidth;
         PIXEL_HEIGHT = TARGET_PIXEL_SIZE +  yRemainder/gridHeight;
-        Log.d("Pixel Width", PIXEL_WIDTH + "");
-        Log.d("Pixel Height", PIXEL_HEIGHT + "");
-        Log.d("Horizontal Pixels", gridWidth + "");
-        Log.d("Vertical Pixels", gridHeight + "");
 
         grid = new GridTile[gridWidth][gridHeight];
     }
 
     protected void redraw(){
+        updateGameState();
         invalidate();
+    }
+
+    protected void updateGameState() throws IndexOutOfBoundsException{
+        if(dir == null){
+            return;
+        }
+        switch(dir){
+            case UP:
+                snakeY = snakeY - 1;
+                break;
+            case LEFT:
+                snakeX = snakeX - 1;
+                break;
+            case RIGHT:
+                snakeX = snakeX + 1;
+                break;
+            case DOWN:
+                snakeY = snakeY + 1;
+                break;
+        }
+
+        clearSnake();
+        spawnSnake(snakeX, snakeY, dir, length);
     }
 }
 
--- Snake/app/src/main/java/com/example/s1300465/snake/GameScreen.java
+++ Snake/app/src/main/java/com/example/s1300465/snake/GameScreen.java
@@ -1,5 +1,6 @@
 package com.example.s1300465.snake;
 
+import android.graphics.Path;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.util.Log;
 public class GameScreen extends AppCompatActivity {
     GameArea gameArea;
     boolean gameRunning = false;
+    Direction direction = Direction.UP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,48 @@ public class GameScreen extends AppCompatActivity {
 
         gameArea = (GameArea) findViewById(R.id.gameArea);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initNewGame();
+            }
+        }, 1000);
+    }
+
+    protected void initNewGame(){
         initGameLoop();
+
+        int spawnX = Math.round(gameArea.getGridWidth()/2);
+        int spawnY = Math.round(gameArea.getGridHeight()/2);
+        gameArea.spawnSnake(spawnX, spawnY, direction, 3);
+    }
+
+    protected void initGameLoop(){
+        final int refreshRate = 500;
+
+        final Handler gameLoop = new Handler();
+        final Runnable refresh = new Runnable() {
+            @Override
+            public void run() {
+                if(gameRunning) {
+                    doGameLoop();
+                    gameLoop.postDelayed(this, refreshRate);
+                }
+            }
+        };
+
+        gameRunning = true;
+
+        refresh.run();
+    }
+
+    protected void doGameLoop(){
+        try {
+            gameArea.redraw();
+        }catch(IndexOutOfBoundsException ex){
+            Log.w("Died", "You died!");
+            gameRunning = false;
+        }
     }
 
     @Override
@@ -29,23 +72,11 @@ public class GameScreen extends AppCompatActivity {
         super.onStop();
         gameRunning = false;
     }
+}
 
-    protected void initGameLoop(){
-        final int refreshRate = 1000;
-
-        final Handler gameLoop = new Handler();
-        final Runnable refresh = new Runnable() {
-            @Override
-            public void run() {
-                if(gameRunning) {
-                    gameArea.redraw();
-                    gameLoop.postDelayed(this, refreshRate);
-                }
-            }
-        };
-
-        gameRunning = true;
-
-        refresh.run();
-    }
+enum Direction{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
 }
