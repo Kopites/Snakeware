f91f67cd58d82f79195fec7bc5a31235a3a56aa3
Murray Coghill <Murreey@users.noreply.github.com>
2016-02-26 15:44:32 +0000

    Grab phone information and store it on first boot up

--- Snake/app/src/main/java/com/example/s1300465/snake/MainActivity.java
+++ Snake/app/src/main/java/com/example/s1300465/snake/MainActivity.java
@@ -1,16 +1,76 @@
 package com.example.s1300465.snake;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.jar.Manifest;
+
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper dbh;
+    TelephonyManager tm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbh = new DatabaseHelper(this);
+
+        if(checkPermissions()){
+            storePhoneNumber();
+        } // else onRequestPermissionsResult will be called
+    }
+
+    public boolean checkPermissions(){
+        ArrayList<String> permissions = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED){
+            permissions.add(android.Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.PROCESS_OUTGOING_CALLS) == PackageManager.PERMISSION_DENIED){
+            permissions.add(android.Manifest.permission.PROCESS_OUTGOING_CALLS);
+        }
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_DENIED){
+            permissions.add(android.Manifest.permission.RECEIVE_SMS);
+        }
+
+        if(permissions.size() == 0) {
+            return true;
+        }
+
+        ActivityCompat.requestPermissions(this, permissions.toArray(new String[permissions.size()]), 0);
+        return false;
+    }
+
+    public void storePhoneNumber(){
+        tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String simSerial = tm.getSimSerialNumber();
+        String operator = tm.getNetworkOperatorName();
+
+        //We save the SIM Serial number rather than phone number because
+        //TelephonyManager.getLine1Number() rarely works for whatever reason
+        //whereas a working phone is guaranteed to have a SIM serial number for ID purposes
+
+        Log.d("SimSerial", simSerial);
+        Log.d("Network", operator);
+        if(simSerial != null && simSerial.length() > 0) {
+            Log.d("Saving", simSerial);
+            dbh.savePhone(simSerial, operator);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if(checkPermissions()){
+            storePhoneNumber();
+        }
     }
 
     public void startNewGame(View v){
