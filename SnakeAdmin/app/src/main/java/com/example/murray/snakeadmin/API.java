4667aeb2a3db88634004b94c05f40f7c9eaf68f5
Murray Coghill <Murreey@users.noreply.github.com>
2016-03-08 10:39:14 +0000

    Implement API fetching in Admin app

---
+++ SnakeAdmin/app/src/main/java/com/example/murray/snakeadmin/API.java
@@ -0,0 +1,98 @@
+package com.example.murray.snakeadmin;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public class API {
+    APIResponse obj;
+    final String URL = "http://mayar.abertay.ac.uk/~1300465/snake";
+
+    public API(APIResponse obj){
+        this.obj = obj;
+    }
+
+    public void fetchPhones(){
+        new APIJSONFetcher(obj).execute(URL + "/get_phones.php");
+    }
+}
+
+class APIJSONFetcher extends AsyncTask<String, String, JSONObject> {
+    APIResponse obj;
+    int status;
+
+    public APIJSONFetcher(APIResponse obj){
+        this.obj = obj;
+    }
+
+    protected JSONObject doInBackground(String[] params){
+        JSONObject result = null;
+        InputStream in;
+        String urlString = params[0];
+
+        try {
+            URL url = new URL(urlString);
+            URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+            url = new URL(uri.toASCIIString());
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            status = urlConnection.getResponseCode();
+            if(status >= HttpURLConnection.HTTP_BAD_REQUEST){
+                in = new BufferedInputStream(urlConnection.getErrorStream());
+            }else{
+                in = new BufferedInputStream(urlConnection.getInputStream());
+            }
+
+            BufferedReader streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            StringBuilder responseStrBuilder = new StringBuilder();
+
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null) {
+                responseStrBuilder.append(inputStr);
+            }
+            result = new JSONObject(responseStrBuilder.toString());
+            in.close();
+        }catch(FileNotFoundException ex){
+            Log.w("API", "File not found! " + urlString);
+        }catch(UnknownHostException ex){
+            Log.w("API", "Couldn't find server - no internet connection?");
+            this.cancel(true);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    protected void onPostExecute(JSONObject result){
+        if(status >= HttpURLConnection.HTTP_BAD_REQUEST){
+            try {
+                Log.w("Bad Request", status + " " + result.getString("message"));
+            }catch(NullPointerException | JSONException ex){
+                ex.printStackTrace();
+            }
+        }
+
+        this.obj.resultsReturned(result);
+    }
+}
+
+
+interface APIResponse {
+    void resultsReturned(JSONObject results);
+}
--- SnakeAdmin/app/src/main/java/com/example/murray/snakeadmin/MainActivity.java
+++ SnakeAdmin/app/src/main/java/com/example/murray/snakeadmin/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.murray.snakeadmin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainActivity extends AppCompatActivity implements APIResponse{
+    ArrayList<String> phones = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new API(this).fetchPhones();
     }
 
     @Override
@@ -33,5 +43,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void resultsReturned(JSONObject results){
+        if(results != null) {
+            try {
+                //Convert the returned JSON list of deviceIDs into an ArrayList
+                Iterator<?> keys = results.keys();
+                while(keys.hasNext()) {
+                    String key = (String) keys.next();
+                    if(results.get(key) instanceof JSONObject){
+                        phones.add(((JSONObject) results.get(key)).getString("deviceID"));
+                    }
+                }
+            }catch(JSONException ex) {
+                Log.d("JSON", "Something went wrong with server data");
+            }
+        }
     }
 }
