4ecf2e0a22be9383e8d281e3bd4f054cf17d9dc7
Murreey <Murreey@users.noreply.github.com>
2016-03-13 21:25:52 +0000

    Fix logging incoming calls - new interceptor object created every time so data isn't saved - store it in shared prefs instead to check when needed

--- Snake/app/src/main/java/com/example/s1300465/snake/IncomingCallInterceptor.java
+++ Snake/app/src/main/java/com/example/s1300465/snake/IncomingCallInterceptor.java
@@ -4,12 +4,11 @@ package com.example.s1300465.snake;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class IncomingCallInterceptor extends BroadcastReceiver {
-    private long startTime;
-    private String incomingNumber;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -19,22 +18,41 @@ public class IncomingCallInterceptor extends BroadcastReceiver {
 
         Log.d("State Change", state);
         if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
-            incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            startTime = System.currentTimeMillis();
+            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            long startTime = System.currentTimeMillis();
+            Log.d("Call", "Call started (ringing)");
+            Log.d("Start Time", startTime + "");
+            Log.d("Incoming Number", incomingNumber + "");
+            SharedPreferences.Editor prefsEditor = context.getSharedPreferences("com.example.s1300465.snake", Context.MODE_PRIVATE).edit();
+            prefsEditor.putLong("callStartTime", startTime);
+            prefsEditor.putString("incomingNumber", incomingNumber);
+            prefsEditor.apply();
         }else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+            long startTime;
+            String incomingNumber;
+
+            SharedPreferences prefs = context.getSharedPreferences("com.example.s1300465.snake", Context.MODE_PRIVATE);
+            startTime = prefs.getLong("callStartTime", 0);
+            incomingNumber = prefs.getString("incomingNumber", null);
+            
+            Log.d("Call", "Call ended (possibly)");
+            Log.d("Start Time", startTime + "");
+            Log.d("Incoming Number", incomingNumber + "");
             if(incomingNumber == null || startTime == 0){
                 //If the phone changed to idle but there's no startTime stored, then it wasn't previously ringing
                 //(probably an outgoing call)
+                Log.d("Call", "Not logged, incomingNumber null or startTime 0");
                 return;
             }
 
             long endTime = System.currentTimeMillis();
             long callDuration = endTime - startTime;
+            Log.d("End Time", endTime + "");
+            Log.d("Duration", callDuration + "");
 
             dbh.savePhoneCall(incomingNumber, false, callDuration, startTime);
 
-            startTime = 0;
-            incomingNumber = null;
+            prefs.edit().remove("callStartTime").remove("incomingNumber").apply();
         }
     }
 }
